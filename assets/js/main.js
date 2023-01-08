76fb4533d251bb8fe7e86fe8f0212ccaaa89ce99
Shalnoi Mottaki <58374132+shalnoimottaki@users.noreply.github.com>
2023-01-08 13:00:33 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,3 +186,12 @@
   });
 
 })(jQuery);
+
+
+function scrollSmoothTo(elementId) {
+  var element = document.getElementById(elementId);
+  element.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth'
+  });
+}
